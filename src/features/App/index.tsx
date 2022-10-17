928b2ef7766b03e687b45402942514c027cc9379
Impartiall <42242696+Impartiall@users.noreply.github.com>
2022-10-17 19:52:32 -0400

    remove unnecessary tab imports from App

--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 import GlobalData from '../../types/GlobalData'
 import CourseBucket from '../CourseBucket'
